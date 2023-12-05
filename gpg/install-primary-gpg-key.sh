0154e2772fcfc44715ed752d8a6f09381ff7ba8d
Alex Creasy <acreasy@redhat.com>
2023-12-05 21:01:53 +0000

    Update

--- gpg/install-primary-gpg-key.sh
+++ gpg/install-primary-gpg-key.sh
@@ -6,3 +6,6 @@ gpg --import "$KEY_FILE"
 
 ## Add finding GPG key below to complete
 #expect -c 'spawn gpg --edit-key {KEY} trust quit; send "5\ry\r"; expect eof'
+
+# Save password in keychain
+echo "test" | gpg --clearsign
